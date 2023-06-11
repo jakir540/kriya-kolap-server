490968013defb53bb1254b9dcb52c7b090eabecb
jakir540 <jakirhossainmd540@gamil.com>
2023-06-11 16:33:38 +0600

    dashbord secuirity functionality added

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 const corsOptions = {
@@ -11,8 +12,31 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200,
 };
+//middleware
 app.use(cors(corsOptions));
 app.use(express.json());
+
+//middleware
+
+const VerifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized-access" });
+  }
+  // bearer token [bearer token]
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized-access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wkiarbk.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -31,23 +55,200 @@ async function run() {
     await client.connect();
     const classCollection = client.db("yogaDB").collection("classes");
     const instructorsCollection = client.db("yogaDB").collection("instructors");
+    const usersCollection = client.db("yogaDB").collection("users");
 
-    //----------------classes api--------------
+    //jwt
 
-    app.get("/classes", async (req, res) => {
-      const cursor =  classCollection.find().sort({students: -1}).limit(6);
-      const result =await cursor.toArray();
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    const VerifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+      next();
+    };
+
+    // verify instructor -------------
+    const VerifyInstructor = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "instructor") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+      next();
+    };
+
+    //------------Users api ------------
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      console.log(existingUser);
+      if (existingUser) {
+        return res.send(existingUser);
+      }
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
       res.send(result);
     });
 
-    
-//instructors api
-app.get("/instructors", async (req, res) => {
-  const cursor =  instructorsCollection.find().sort({enrollment: -1}).limit(6);
-  const result =await cursor.toArray();
-  res.send(result);
-});
+    // get all user from mongodb
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
+    // make admin----------------
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // check admin and verify admin
+
+    app.get("/users/admin/:email", VerifyJwt, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        return res.send({ admin: false });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
+    // make instructor----------------------------
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const updatedDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // check instructor -------------------------------------
+    app.get("/users/instructor/:email", VerifyJwt, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        return res.send({ instructor: false });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === "instructor" };
+      res.send(result);
+    });
+    // ----------------classes api--------------
+    // for home page only six popular class
+    app.get("/classes", async (req, res) => {
+      const cursor = classCollection.find().sort({ seats: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+    // for home page all  approved class
+    app.get("/approvedClasses", async (req, res) => {
+      
+      const query = {status:"approved"}
+      const cursor = classCollection.find(query)
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+
+
+    // form admin page all classes
+    app.get("/instructor/classes", async (req, res) => {
+      const cursor = classCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/classes", async (req, res) => {
+      const newClass = req.body;
+      const result = await classCollection.insertOne(newClass);
+      res.send(result);
+    });
+
+
+
+    // update class status
+
+    app.patch("/classes/:id", async (req, res) => {     
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: "approved",
+        },
+      };
+      const result = await classCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
+
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //instructors api
+    app.get("/instructors", async (req, res) => {
+      const cursor = instructorsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
